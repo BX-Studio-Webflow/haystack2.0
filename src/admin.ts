705530f84ff72e8e410c41bfd2c0c8fb2899d68a
personal <shedrackitulah@gmail.com>
2024-05-13 22:54:20 +0100

    feat: add image upload to ckeditor

--- src/admin.ts
+++ src/admin.ts
@@ -335,7 +335,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     const sourceDocuments = new Choices(sourceDocumentsInput, {
       removeItemButton: true,
     });
-    const insightDetails = ClassicEditor.create(insightDetailsInput, {});
+    const insightDetails = ClassicEditor.create(insightDetailsInput, {
+      extraPlugins: [ MyCustomUploadAdapterPlugin ]
+    });
 
     insightDetails.then((value) => {
       value.model.document.on("change:data", () => {
@@ -693,6 +695,84 @@ document.addEventListener("DOMContentLoaded", async () => {
       .replace(/^-+/, "")
       .replace(/-+$/, "");
   }
+  class MyUploadAdapter {
+    constructor( loader ) {
+        // The file loader instance to use during the upload.
+        this.loader = loader;
+    }
+
+    // Starts the upload process.
+    upload() {
+        return this.loader.file
+            .then( file => new Promise( ( resolve, reject ) => {
+                this._initRequest();
+                this._initListeners( resolve, reject, file );
+                this._sendRequest( file );
+            } ) );
+    }
+
+    // Aborts the upload process.
+    abort() {
+        if ( this.xhr ) {
+            this.xhr.abort();
+        }
+    }
+
+    // Initializes the XMLHttpRequest object using the URL passed to the constructor.
+    _initRequest() {
+        const xhr = this.xhr = new XMLHttpRequest();
+
+        xhr.open( 'POST', 'https://xhka-anc3-3fve.n7c.xano.io/api:OsMcE9hv/image_upload', true );
+        xhr.responseType = 'json';
+    }
+
+    // Initializes XMLHttpRequest listeners.
+    _initListeners( resolve, reject, file ) {
+        const xhr = this.xhr;
+        const loader = this.loader;
+        const genericErrorText = `Couldn't upload file: ${ file.name }.`;
+
+        xhr.addEventListener( 'error', () => reject( genericErrorText ) );
+        xhr.addEventListener( 'abort', () => reject() );
+        xhr.addEventListener( 'load', () => {
+            const response = xhr.response;
+
+            if ( !response || response.error ) {
+                return reject( response && response.error ? response.error.message : genericErrorText );
+            }
+
+            resolve( {
+                default: response.url
+            } );
+        } );
+
+        if ( xhr.upload ) {
+            xhr.upload.addEventListener( 'progress', evt => {
+                if ( evt.lengthComputable ) {
+                    loader.uploadTotal = evt.total;
+                    loader.uploaded = evt.loaded;
+                }
+            } );
+        }
+    }
+
+    // Prepares the data and sends the request.
+    _sendRequest( file ) {
+        // Prepare the form data.
+        const data = new FormData();
+
+        data.append( 'upload', file );
+
+        // Send the request.
+        this.xhr.send( data );
+    }
+}
+function MyCustomUploadAdapterPlugin( editor ) {
+  editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+      // Configure the URL to the upload script in your back-end here!
+      return new MyUploadAdapter( loader );
+  };
+}
 });
 
 interface GetEditorInsightsResponse {
